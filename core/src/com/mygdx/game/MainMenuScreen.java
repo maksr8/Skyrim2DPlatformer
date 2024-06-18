52a99b4cc134692f500b8b2d516e7d033141ff02
frice27 <162634675+frice27@users.noreply.github.com>
2024-06-18 09:02:43 +0300

    Update MainMenuScreen.java
    added buttons

--- core/src/com/mygdx/game/MainMenuScreen.java
+++ core/src/com/mygdx/game/MainMenuScreen.java
@@ -3,9 +3,18 @@ package com.mygdx.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import helper.Assets;
 
 public class MainMenuScreen extends ScreenAdapter {
@@ -14,15 +23,58 @@ public class MainMenuScreen extends ScreenAdapter {
     private SpriteBatch batch;
     private BitmapFont font;
     private Assets assets;
-    private boolean isSelectingLevel;
+    private Stage stage;
+    private TextButton.TextButtonStyle textButtonStyle;
 
     public MainMenuScreen(final Skyrim2DGame game) {
         this.game = game;
         this.camera = game.getOrthographicCamera();
         this.batch = game.getBatch();
         this.assets = game.getAssets();
-        this.isSelectingLevel = false;
         this.font = new BitmapFont();
+
+
+        stage = new Stage(new ScreenViewport());
+
+
+        textButtonStyle = new TextButton.TextButtonStyle();
+        textButtonStyle.font = new BitmapFont();
+        textButtonStyle.up = createDrawable("buttons/button_up.png"); // Make sure you have this texture
+        textButtonStyle.down = createDrawable("buttons/button_down.png"); // Make sure you have this texture
+
+
+        TextButton startButton = new TextButton("", textButtonStyle);
+        startButton.setSize(200, 60);
+        startButton.setPosition((game.getGameWidth() - startButton.getWidth()) / 2, game.getGameHeight() / 2 + 20);
+        startButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                game.setScreen(new GameScreen(game, 1));
+                dispose();
+            }
+        });
+
+        // Create Exit button
+        TextButton exitButton = new TextButton("", textButtonStyle);
+        exitButton.setSize(200, 60);
+        exitButton.setPosition((game.getGameWidth() - exitButton.getWidth()) / 2, game.getGameHeight() / 2 - 60);
+        exitButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.exit();
+            }
+        });
+
+        stage.addActor(startButton);
+        stage.addActor(exitButton);
+
+
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    private Drawable createDrawable(String filePath) {
+        Texture texture = new Texture(Gdx.files.internal(filePath));
+        return new TextureRegionDrawable(texture);
     }
 
     @Override
@@ -33,13 +85,13 @@ public class MainMenuScreen extends ScreenAdapter {
         batch.setProjectionMatrix(camera.combined);
 
         batch.begin();
-        if (isSelectingLevel) {
-
-        } else {
-            batch.draw(assets.manager.get(assets.gameNameLabel), game.getGameWidth() / 2 - 400, game.getGameHeight() / 2 + 100, 800, 250);
-            font.draw(batch, "Tap anywhere to begin!", 20, 20);
-        }
+        batch.draw(assets.manager.get(assets.gameNameLabel), game.getGameWidth() / 2 - 400, game.getGameHeight() / 2 + 100, 800, 250);
+        font.draw(batch, "Tap anywhere to begin!", 20, 20);
         batch.end();
+
+
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
 
         handleInput();
     }
@@ -55,5 +107,11 @@ public class MainMenuScreen extends ScreenAdapter {
     public void show() {
         camera.zoom = 1;
         camera.position.set(game.getGameWidth() / 2, game.getGameHeight() / 2, 0);
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+        font.dispose();
     }
 }
